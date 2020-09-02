b3276bef04a5e71b64a1ce5b16d04abbe6addda1
Harley Garcia <hagarcia99@outlook.com>
2020-09-01 20:39:45 -0500

    Days of the week and ejs

--- app.js
+++ app.js
@@ -4,12 +4,47 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) { 
-    res.sendFile(__dirname + "/index.html");
+
+
+app.get("/", function (req, res) {
+
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Santurday";
+            break;
+        default:
+            console.log("ERROR, no such day exists " + currentDay);
+    }
+
+
+    res.render("list", { kindOfDay: day });
+
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
