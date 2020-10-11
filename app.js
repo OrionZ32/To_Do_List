fad451a897e90072cbcacee8220e47c387b81905
Harley Garcia <hagarcia99@outlook.com>
2020-10-11 10:26:26 -0500

    Reformat code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -11,32 +11,31 @@ let items = ["Fix RSX", "Code", "Play Games"];
 let workItems = [];
 
 app.get("/", function (req, res) {
-    let day = date.getDate();
+  let day = date.getDate();
 
-    res.render("list", { listTitle: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
-app.post("/", function(req, res) {
-    let item = req.body.newItem;
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
 
-    if(req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-        items.push(item);
-        res.redirect("/");
-    }
-
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
-app.get("/work", function(req, res) {
-    res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res) {
-    res.render("about");
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function () {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
